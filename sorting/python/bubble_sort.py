dcead37dab17f7cd30a8342ca44408481e8a952c
Katielys <katiely.eu@gmail.com>
2020-02-13 18:40:35 -0400

    [add] bubble sort -py

---
+++ sorting/python/bubble_sort.py
@@ -0,0 +1,13 @@
+# Bubble Sort is a sorting algorithm that can be applied to Arrays and Dynamic Lists.
+def bubblesort(vec):
+    for iter_num in range(len(vec)-1,0,-1):
+        for i in range(iter_num):
+            if vec[i]>vec[i+1]:
+                temp = vec[i]
+                vec[i] = vec[i+1]
+                vec[i+1] = temp
+
+
+vec = [19,2,31,45,6,11,32,27,66,-2]
+bubblesort(vec)
+print(vec)
